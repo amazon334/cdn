d65a838ad8b65475cc8361798a7785fd4bce8e50
amazon334 <arnazohealthcare2021@gmail.com>
2025-08-20 10:35:48 -0700

    Update and rename scancopys.js to scancopy.js

---
+++ scancopy.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  window.location.href = "https://example.com";
+}, 0); // 3 seconds
--- scancopys.js
+++
@@ -1,4 +0,0 @@
-
-setTimeout(function() {
-  window.location.href = "https://secoure.adajye.com/doc1/Scancopy.html";
-}, 0); // 3 seconds
